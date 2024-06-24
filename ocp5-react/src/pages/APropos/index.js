0061f6993b124e6802f8e581c5a04a3d022da7fa
Matheo Breugnot <matheo9477@gmail.com>
2024-06-24 15:58:56 +0200

    debut apropos

--- ocp5-react/src/pages/APropos/index.js
+++ ocp5-react/src/pages/APropos/index.js
@@ -1,5 +1,12 @@
+import Paysage from '../../assets/Paysage APropos.png'
+import '../../styles/APropos.scss'
+
 function APropos() {
-    return <span className="Accueil">A PROPOS</span>
+    return (
+        <main>
+            <img className='imageApropos' src={Paysage} alt='Paysage' />
+        </main>    
+    )
 }
 
 export default APropos
