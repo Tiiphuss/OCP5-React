fde54191a5031ae4ff136c5ddb7889a369df2321
Matheo Breugnot <matheo9477@gmail.com>
2024-08-05 14:40:50 +0200

    Slideshow fini

--- ocp5-react/src/components/Slideshow/index.js
+++ ocp5-react/src/components/Slideshow/index.js
@@ -1,21 +1,33 @@
-import { Logements } from "../../datas/logements.js"
 import { useState } from "react"
 import "../../styles/Slideshow.scss"
 import Fleche_gauche from "../../assets/flecheGauche.png"
 
 function Slideshow({imgLogement}) {
     const [currentSlide, updateSlide] = useState(0)
-    const prevSlide = (currentSlide - 1 + imgLogement.length) % imgLogement.length;
-    const nextSlide = (currentSlide + 1 + imgLogement.length) % imgLogement.length;
+    const prevSlide = currentSlide - 1;
+    const nextSlide = currentSlide + 1;
+    const nonUnique = imgLogement.length > 1
+    console.log(imgLogement.length)
+    
+    if (currentSlide === imgLogement.length) {
+        updateSlide(0)
+    }
+    if (currentSlide === -1) {
+        updateSlide(imgLogement.length - 1)
+    }
 
-    return (
+    return nonUnique? (
         <>
-            <img src={imgLogement[currentSlide]} alt="slideshow images"></img>
+        <main>
+            <img className="photo" src={imgLogement[currentSlide]} alt="slideshow images"></img>
             <button className="flecheGauche" onClick={() => updateSlide(prevSlide)}> <img src={Fleche_gauche} alt="flèche gauche"></img> </button>
             <button className="flecheDroite" onClick={() => updateSlide(nextSlide)}> <img src={Fleche_gauche} alt="flèche droite"></img> </button>
-            <p>{currentSlide} / {imgLogement.length} </p>
+            <p className="numero">{currentSlide + 1} / {imgLogement.length} </p>
+        </main>         
         </>
-    )
+    ) : <main>
+            <img src={imgLogement[currentSlide]} alt="slideshow images"></img>
+        </main>
 }
 
 export default Slideshow
